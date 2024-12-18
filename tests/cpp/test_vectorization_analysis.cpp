624dfe2528dcfd29ae0491fbeaa785dc2c7f6a66
jjsjann123 <alex.jann2012@gmail.com>
2024-12-18 10:45:29 -0800

    WIP

--- tests/cpp/test_vectorization_analysis.cpp
+++ tests/cpp/test_vectorization_analysis.cpp
@@ -25,7 +25,7 @@ void checkMappedVal(const std::unordered_map<TensorView*, Val*>& map, TensorView
   auto iter = map.find(tv_target);
   EXPECT_TRUE(iter != map.end());
   if (iter != map.end()) {
-    EXPECT_EQ(iter->evaluate(), val);
+    EXPECT_EQ(iter.second->evaluate(), val);
   }
 }
 
@@ -37,11 +37,10 @@ using VectorizationAnalysisTest = NVFuserTest;
 TEST_F(VectorizationAnalysisTest, ContigInnerDimsMapperResizeFastestDimensionP2C) {
   Fusion fusion;
   FusionGuard fg(&fusion);
+  std::vector<std::pair<TensorView*, int64_t>> expection_list;
 
   auto tv0 = makeContigConcreteTensor({4, 8, 16});
   fusion.addInput(tv0);
-
-  std::vector<std::pair<TensorView*, int64_t>> expection_list;
 
   // positive resize (+2, +2)
   auto inner_pos = pad(tv0, {IrBuilder::create<Val>(2L), IrBuilder::create<Val>(2L)});
@@ -69,9 +68,9 @@ TEST_F(VectorizationAnalysisTest, ContigInnerDimsMapperResizeFastestDimensionP2C
   fusion.addOutput(inner_neg_uneven);
 
   // negative large resize to zero (-8, -8)
-  auto inner_neg = pad(tv0, {IrBuilder::create<Val>(-8L), IrBuilder::create<Val>(-8L)});
-  expection_list.emplace_back(std::make_pair(inner_neg, 0));
-  fusion.addOutput(inner_neg);
+  auto inner_neg_large = pad(tv0, {IrBuilder::create<Val>(-8L), IrBuilder::create<Val>(-8L)});
+  expection_list.emplace_back(std::make_pair(inner_neg_large, 0));
+  fusion.addOutput(inner_neg_large);
 
   // uneven resize (-2, 4)
   auto inner_uneven = pad(tv0, {IrBuilder::create<Val>(-2L), IrBuilder::create<Val>(4L)});
@@ -86,53 +85,83 @@ TEST_F(VectorizationAnalysisTest, ContigInnerDimsMapperResizeFastestDimensionP2C
   std::unordered_map<TensorView*, Val*> projected_extent_map = vectorize_helper::ContiguousInnerDimensionsMapper::map(tv0, tv0->getLogicalDomain()).getTvToContigMergeOfInnerSizeMap();
 
   for (const auto& [tv, val] : expection_list) {
-    checkMappedVal(projected_extent_map, , val);
+    checkMappedVal(projected_extent_map, tv, val);
   }
 }
 
 TEST_F(VectorizationAnalysisTest, ContigInnerDimsMapperResizeMiddleDimension) {
   Fusion fusion;
   FusionGuard fg(&fusion);
+  std::vector<std::pair<TensorView*, int64_t>> expection_list;
 
   auto tv0 = makeContigConcreteTensor({4, 8, 16});
   fusion.addInput(tv0);
 
-  std::vector<std::pair<TensorView*, int64_t>> expection_list;
-
   // positive resize (+2, +2)
   auto middle_pos = pad(tv0, {IrBuilder::create<Val>(0L), IrBuilder::create<Val>(0L), IrBuilder::create<Val>(2L), IrBuilder::create<Val>(2L)});
-  expection_list.emplace_back(std::make_pair(inner_one_size, 2*16));
+  expection_list.emplace_back(std::make_pair(middle_pos, 2*16));
   fusion.addOutput(middle_pos);
 
   // negative resize (-2, -2)
   auto middle_neg = pad(tv0, {IrBuilder::create<Val>(0L), IrBuilder::create<Val>(0L), IrBuilder::create<Val>(-2L), IrBuilder::create<Val>(-2L)}); 
-  expection_list.emplace_back(std::make_pair(inner_one_size, 2*16));
+  expection_list.emplace_back(std::make_pair(middle_neg, 2*16));
   fusion.addOutput(middle_neg);
 
   std::unordered_map<TensorView*, Val*> projected_extent_map = vectorize_helper::ContiguousInnerDimensionsMapper::map(tv0, tv0->getLogicalDomain()).getTvToContigMergeOfInnerSizeMap();
   for (const auto& [tv, val] : expection_list) {
-    checkMappedVal(projected_extent_map, , val);
+    checkMappedVal(projected_extent_map, tv, val);
   }
 }
 
-// TEST_F(VectorizationAnalysisTest, ContigInnerDimsMapperResizeStacked) {
-//   Fusion fusion;
-//   FusionGuard fg(&fusion);
-// 
-//   auto tv0 = makeContigConcreteTensor({4, 8, 16});
-//   fusion.addInput(tv0);
-//   /////////////////
-//   // stacked resize
-//   /////////////////
-//   // resize on different dimension
-// 
-//   // resize on the same dimension
-// 
-//   // resize on the same dimension, squeeze size to zero
-// 
-//   std::unordered_map<TensorView*, Val*> projected_extent_map = vectorize_helper::ContiguousInnerDimensionsMapper::map(tv0, tv0->getLogicalDomain()).getTvToContigMergeOfInnerSizeMap();
-// 
-//   std::cout << projected_extent_map[inner_pos]->evaluate() << std::endl;
-// }
+TEST_F(VectorizationAnalysisTest, ContigInnerDimsMapperResizeMultipleDimension) {
+  Fusion fusion;
+  FusionGuard fg(&fusion);
+
+  auto tv0 = makeContigConcreteTensor({4, 8, 36});
+  fusion.addInput(tv0);
+
+  auto tv1 = pad(tv2, {IrBuilder::create<Val>(4L), IrBuilder::create<Val>(4L), IrBuilder::create<Val>(8L), IrBuilder::create<Val>(8L)}); 
+  fusion.addOutput(tv1);
+
+  std::unordered_map<TensorView*, Val*> projected_extent_map_from_producer = vectorize_helper::ContiguousInnerDimensionsMapper::map(tv0, tv0->getLogicalDomain()).getTvToContigMergeOfInnerSizeMap();
+  checkMappedVal(projected_extent_map_from_producer, tv1, 8);
+
+  std::unordered_map<TensorView*, Val*> projected_extent_map_from_consumer = vectorize_helper::ContiguousInnerDimensionsMapper::map(tv1, tv1->getLogicalDomain()).getTvToContigMergeOfInnerSizeMap();
+  checkMappedVal(projected_extent_map_from_consumer, tv0, 8);
+}
+
+TEST_F(VectorizationAnalysisTest, ContigInnerDimsMapperResizeStacked) {
+  Fusion fusion;
+  FusionGuard fg(&fusion);
+  std::vector<std::pair<TensorView*, int64_t>> expection_list;
+
+  auto tv0 = makeContigConcreteTensor({4, 8, 36});
+  fusion.addInput(tv0);
+  /////////////////
+  // stacked resize
+  /////////////////
+  // resize on different dimension
+  auto tv1 = pad(tv0, {IrBuilder::create<Val>(0L), IrBuilder::create<Val>(0L), IrBuilder::create<Val>(0L), IrBuilder::create<Val>(0L), IrBuilder::create<Val>(-2L), IrBuilder::create<Val>(-2L)}); 
+  auto tv2 = pad(tv1, {IrBuilder::create<Val>(0L), IrBuilder::create<Val>(0L), IrBuilder::create<Val>(-2L), IrBuilder::create<Val>(-2L)}); 
+  expection_list.emplace_back(std::make_pair(tv2, 2*12));
+  fusion.addOutput(tv2);
+
+  // resize on the same dimension, squeeze size to zero
+  auto tv3 = pad(tv0, {IrBuilder::create<Val>(-9L), IrBuilder::create<Val>(-9L)});
+  auto tv4 = pad(tv3, {IrBuilder::create<Val>(-9L), IrBuilder::create<Val>(-9L)});
+  expection_list.emplace_back(std::make_pair(tv4, 0));
+  fusion.addOutput(tv4);
+
+  // resize on the same dimension
+  auto tv5 = pad(tv0, {IrBuilder::create<Val>(-6L), IrBuilder::create<Val>(-6L)});
+  auto tv6 = pad(tv5, {IrBuilder::create<Val>(9L), IrBuilder::create<Val>(9L)});
+  expection_list.emplace_back(std::make_pair(tv6, 3));
+  fusion.addOutput(tv6);
+
+  std::unordered_map<TensorView*, Val*> projected_extent_map = vectorize_helper::ContiguousInnerDimensionsMapper::map(tv0, tv0->getLogicalDomain()).getTvToContigMergeOfInnerSizeMap();
+  for (const auto& [tv, val] : expection_list) {
+    checkMappedVal(projected_extent_map, tv, val);
+  }
+}
 
 } // nvfuser
